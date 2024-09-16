7cd5c4707f9e5e6063aa4c90450b57c4f6bc6b69
bushra206 <bushramaham2002@gmail.com>
2024-09-16 13:44:06 +0500

    mobnavbar links height fixed

--- src/components/MobNavBarLinks.jsx
+++ src/components/MobNavBarLinks.jsx
@@ -35,9 +35,9 @@ function MobNavBarLinks({ showmenuicon, setShowMenuIcon }) {
       {showmenuicon && (
         <div
           data-aos="fade-right"
-          className="bg-[#fff] py-3 border-r-[1px]  border-r-[#000] flex w-[94vw] h-[100vh] absolute top-0 left-0 z-50 uppercase overflow-y-scroll  lg:hidden"
+          className=" py-[20px] border-r-[1px]  border-r-[#000] flex w-[94vw] h-[100vh] absolute top-0 left-0 z-50 uppercase   lg:hidden"
         >
-          <div className="flex flex-col justify-start items-center gap-12 m-0  py-4 text-center max-auto h-[100%] z-50 bg-[#fff] w-[80vw] ">
+          <div className="flex flex-col justify-start items-center gap-12 m-0  py-4 text-center max-auto h-[100%] z-50 bg-[#fff] w-[80vw] overflow-y-scroll ">
             <div className="flex gap-3 w-[80%]">
               <div className="w-[33%] h-[80px] flex flex-col gap-1 justify-center items-center border-[1px] border-[#000] rounded-lg">
                 <FaRegCircleUser size={40} />
